80ee7b3d2e39aeb29cde8a3dc62b5dc464476973
Mia2013 <komlosnikolett@gmail.com>
2022-11-04 14:48:53 +0100

    make cakebox responsive

--- frontend/src/components/Shop/BuyProduct.js
+++ frontend/src/components/Shop/BuyProduct.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Alert from "../Alert";
 import { useLoggedInUserContext } from "../LoggedInUserContextProvider";
 
-
 export default function BuyProduct({ id, price }) {
   const { loggedInUser } = useLoggedInUserContext();
 
@@ -67,45 +66,46 @@ export default function BuyProduct({ id, price }) {
 
   return (
     <div>
-      <p>1 szelet: {price} Ft</p>
-      {loggedInUser.userId && 
-      <div>
-      <p>
-        {formData.slice} szelet: {formData.slice * price} Ft
-      </p>
-      <form onSubmit={handleOnSubmit}>
-        <div >
-          <button
-            className="btn mx-2"
-            type="button"
-            onClick={(e) => handleOnClick(e)}
-            value="-1"
-          >
-            -
-          </button>
-          <h4 className="d-inline">{formData.slice} </h4>
-          <button
-            className="btn mx-2"
-            type="button"
-            onClick={(e) => handleOnClick(e)}
-            value="1"
-          >
-            +
-          </button>
-    
-          <button className="bi bi-cart btn d-inline" type="submit" >
-            {" "}
-            Kosárba teszem
-          </button>
+      <p className="ms-3">1 szelet: {price} Ft</p>
+      {loggedInUser.userId && (
+        <div>
+          <p className="ms-3">
+            {formData.slice} szelet: {formData.slice * price} Ft
+          </p>
+          <form onSubmit={handleOnSubmit}>
+            <div>
+              <button
+                className="btn mx-2"
+                type="button"
+                onClick={(e) => handleOnClick(e)}
+                value="-1"
+              >
+                -
+              </button>
+              <h4 className="d-inline">{formData.slice} </h4>
+              <button
+                className="btn mx-2"
+                type="button"
+                onClick={(e) => handleOnClick(e)}
+                value="1"
+              >
+                +
+              </button>
+
+              <button className="bi bi-cart btn d-inline" type="submit">
+                {" "}
+                Kosárba teszem
+              </button>
+            </div>
+            {alertMessage.value && (
+              <Alert
+                className={alertMessage.className}
+                value={alertMessage.value}
+              />
+            )}
+          </form>
         </div>
-        {alertMessage.value && (
-          <Alert
-            className={alertMessage.className}
-            value={alertMessage.value}
-          />
-        )}
-      </form>
-    </div>}
+      )}
     </div>
   );
 }
--- frontend/src/components/Shop/CakeBox.js
+++ frontend/src/components/Shop/CakeBox.js
@@ -3,19 +3,18 @@ import BuyProduct from "./BuyProduct";
 export default function CakeBox({ id, name, description, price, allergenic }) {
   return (
     <div className="box">
-      <img src={`${process.env.PUBLIC_URL}/assets/cakes/${name}.jpg`} />
+      <h3 className="ms-5">{name}</h3>
+      <img src={`${process.env.PUBLIC_URL}/assets/cakes/${name}.jpg`} alt="" />
 
       <div className="content">
-        <h3>{name}</h3>
         <p className="description">{description}</p>
-      
-   
+
         <BuyProduct id={id} price={price} />
       </div>
       <details className="text-start ms-5">
-          <summary>Allergének</summary>
-          <p className="description2">{allergenic}</p>
-        </details>
+        <summary>Allergének</summary>
+        <p className="description2">{allergenic}</p>
+      </details>
     </div>
   );
 }
